67ca22a51bcac357f45d5bb9ffabf996696b37f2
tumbleweeding <cloneop800@gmail.com>
2018-12-06 20:47:41 -0700

    change mousekey settings in tumble config for Iris

--- keyboards/iris/keymaps/tumble/config.h
+++ keyboards/iris/keymaps/tumble/config.h
@@ -34,7 +34,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_FORCE_HOLD
 
-#undef MOUSEKEY_TIME_TO_MAX
-#define MOUSEKEY_TIME_TO_MAX 20
+#define MOUSEKEY_INTERVAL       20
+#define MOUSEKEY_DELAY          0
+#define MOUSEKEY_TIME_TO_MAX    20
+#define MOUSEKEY_MAX_SPEED      7
+#define MOUSEKEY_WHEEL_DELAY 0
 
 #endif
